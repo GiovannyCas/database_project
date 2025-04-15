14da932298058ea419fc4bc043afe820dbe16985
csgiova <gacastrorosa@student.fullsail.edu>
2025-04-15 01:35:25 -0400

    added hashtable for the database

--- Makefile
+++ Makefile
@@ -1,16 +1,25 @@
 C++ = g++
 CFLAGS = -g
 RM = rm -f
+OBJS = hashtable.o
 
 
 
 all: Server Client
 
-Server: server.cpp
-	$(C++) -std=c++11 -Wall -Wextra -O2 $(CFLAGS) server.cpp -o server 
+
+
+Server: server.o $(OBJS)
+	$(C++) -std=c++11 -Wall -Wextra -O2 $(CFLAGS) -o server server.o $(OBJS)
 
 Client: client.cpp
 	$(C++) -std=c++11 -Wall -Wextra -O2 $(CFLAGS) client.cpp -o client
+
+server.o: server.cpp hashtable.h
+	$(C++) -std=c++11 -Wall -Wextra -O2 $(CFLAGS) -c server.cpp
+
+hashtable.o: hashtable.cpp hashtable.h
+	$(C++) -std=c++11 -Wall -Wextra -O2 $(CFLAGS) -c hashtable.cpp
 
 clean: 
 	$(RM) server
---
+++ hashtable.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+// hashtabl node, should be emmbedded in thr payload.
+struct HNode
+{
+    HNode *next = NULL;
+    uint64_t hcode= 0;
+};
+
+// a simple fixed-sized hashtable
+struct HTab
+{
+    HNode **tab = NULL;     //array of slot
+    size_t mask = 0;        //power of 2 array size, 2^n - 1
+    size_t size = 0;        // number of keys
+};
+
+// the real hashtable inteface.
+// it uses 2 hashtable for progressive rehashing.
+struct HMap
+{
+    HTab newer;
+    HTab older;
+    size_t mirgate_pos = 0;
+};
+
+HNode *hm_lookup(HMap *hmap, HNode *key, bool(*eq) (HNode *, HNode *));
+void hm_insert(HMap *hmap, HNode *node);
+HNode *hm_delete(HMap *hmap, HNode *node, bool (*eq)(HNode *, HNode *));
+void hm_clear(HMap *hmap);
+size_t hm_size(HMap *hmap);
+    
